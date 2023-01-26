3b04da18ae9cbe7960fdd8b797459b28f9a5b68e
carlesbarreda <9409275+carlesbarreda@users.noreply.github.com>
2023-01-26 01:01:48 +0100

    3 files modified

--- src/plugins/locales/i18next-vue.ts
+++ src/plugins/locales/i18next-vue.ts
@@ -1,6 +1,7 @@
 import i18next from 'i18next';
 import I18NextVue from 'i18next-vue';
 //import Backend from 'i18next-fs-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import locales from '@intlify/unplugin-vue-i18n/messages';
 
@@ -17,18 +18,24 @@ export const messages = {
 };
 
 //.use(Backend)
-i18next.init({
-  // debug: true,
-  initImmediate: false,
-  fallbackLng: 'en',
-  lng: 'ca',
-  resources: messages,
-  /*
+i18next
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    debug: true,
+    initImmediate: false,
+    fallbackLng: 'en',
+    //lng: 'ca',
+    resources: messages,
+    /*
     resources: {
       ...
     },
-  */
-  /*
+    */
+    /*
     preload: readdirSync(join(__dirname, '../locales')).filter((fileName) => {
       const joinedPath = join(join(__dirname, '../locales'), fileName)
       const isDirectory = lstatSync(joinedPath).isDirectory()
@@ -40,7 +47,7 @@ i18next.init({
       loadPath: join(__dirname, '../locales/{{lng}}/{{ns}}.json')
     }
     */
-});
+  });
 
 export function registerI18NextVue(app: App) {
   app.use(I18NextVue, { i18next });
